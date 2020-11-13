3d2e72ad9967c1048ebb350f1683a614fd1c8ebd
root <root@conun.io>
2020-11-13 09:37:46 +0900

    adding websocket

--- src/index.js
+++ src/index.js
@@ -4,28 +4,36 @@ const Node = require('./libp2p_bundle')
 const {stdinTostream, streamToConsole} = require('./stream')
 const express = require('express')
 const bodyParser = require('body-parser')
-
-const app  = express()
-
-
-
-app.use(bodyParser.json)
-app.use(bodyParser.urlencoded({extended: true}))
+const http = require('http').createServer(app)
+const io = require('socket.io')(http)
 
 
+const PORT = 8080
+var app  = express()
 
-app.get('/user', async(req,res)=> {
+
+app.use(express.static(__dirname + '/public'))
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+// app.use(bodyParser.json)
+// app.use(bodyParser.urlencoded({extended: true}))
+
+
+
+app.get('/signup', async(req,res)=> {
     const genID = await PeerId.create({bits:1024, keyType: 'rsa'})
     res.send(JSON.stringify(genID.toJSON(), null, 2))
 })
 
-app.get('/', (req,res)=> {
-    res.send({"hey": "hello"})
+
+
+app.get('/chat', (req,res)=> {
+    res.send({"status": "healthy"})
+    res.send({"new": "newMessage"})
 })
 
 
 
-app.listen(3000, function(err){ 
-    if (err) console.log("Error in server setup") 
-    console.log("Server listening on Port", 3000); 
+app.listen(PORT, ()=> {  
+    console.log("Server listening on Port", PORT); 
 })
